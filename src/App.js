52b8ff3391b19c9dcccd9f1a71bb52caa6879e7e
eomttt <hyunt0413@gmail.com>
2020-04-12 01:15:46 +0900

    Private node module test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 // Props to child
-import ParentComp from './components/childProps/ParentComp';
-import ChildComp from './components/childProps/ChildComp';
+// import ParentComp from './components/childProps/ParentComp';
+// import ChildComp from './components/childProps/ChildComp';
 
 // MulitiSelector
-import MultiSelector from './components/multiSelector/MultiSelector';
-import TestMultiCheckItem from './components/multiSelector/TestMultiCheckItem';
+// import MultiSelector from './components/multiSelector/MultiSelector';
+// import TestMultiCheckItem from './components/multiSelector/TestMultiCheckItem';
+// import ReactTreeMultiSelector from 'react-tree-multi-selector';
+import ReactTreeMultiSelector from './test';
+// import MultiSelector from 'multi-select-react';
 import TestMultiCategories, { initialSelectedIds, exceptionIds, notSelectionIds } from './components/multiSelector/TestMultiCategories';
 
 // Props to components
-import PropsComp from './components/propsComponent/PropsComp';
+// import PropsComp from './components/propsComponent/PropsComp';
 
 // Redux & Redux-Thunk test
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import createSagaMiddleware from 'redux-saga';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import ReduxThunk from 'redux-thunk';
+// import createSagaMiddleware from 'redux-saga';
 
-import CountComponent from './components/reduxThunkTest/Count';
-import CountFCComponent from './components/reduxThunkTest/CountFC';
+// import CountComponent from './components/reduxThunkTest/Count';
+// import CountFCComponent from './components/reduxThunkTest/CountFC';
 
-import reducers from './reducers';
-import sagas from './sagas';
+// import reducers from './reducers';
+// import sagas from './sagas';
 
-// Apollo
-import { ApolloProvider } from 'react-apollo';
-// Apollo boost
-import ApolloClient, { gql } from 'apollo-boost';
+// // Apollo
+// import { ApolloProvider } from 'react-apollo';
+// // Apollo boost
+// import ApolloClient, { gql } from 'apollo-boost';
 
 // Apollo client
 // import { ApolloClient } from 'apollo-client';
@@ -37,23 +40,23 @@ import ApolloClient, { gql } from 'apollo-boost';
 // import { createHttpLink } from "apollo-link-http";
 // import gql from "graphql-tag";  
 
-import ApolloTest from './components/apolloTest/apolloTest';
-import ApolloTestDelay from './components/apolloTest/apolloTestDelay';
-import ApolloTestQuery from './components/apolloTest/apolloTestQuery';
+// import ApolloTest from './components/apolloTest/apolloTest';
+// import ApolloTestDelay from './components/apolloTest/apolloTestDelay';
+// import ApolloTestQuery from './components/apolloTest/apolloTestQuery';
 
-import ApolloTestMutation from './components/apolloTest/apolloTestMutaion';
-import ApolloTestMutationUpdate from './components/apolloTest/apolloTestMutaionUpdate';
+// import ApolloTestMutation from './components/apolloTest/apolloTestMutaion';
+// import ApolloTestMutationUpdate from './components/apolloTest/apolloTestMutaionUpdate';
 
-import ApolloTestQueryAndMutation from './components/apolloTest/apolloTestQueryAndMutation';
-import ApolloTestQueryAndMutationUpdate from './components/apolloTest/apolloTestQueryAndMutationUpdate';
+// import ApolloTestQueryAndMutation from './components/apolloTest/apolloTestQueryAndMutation';
+// import ApolloTestQueryAndMutationUpdate from './components/apolloTest/apolloTestQueryAndMutationUpdate';
 
 // Epub
-import EpubReader from './components/epubReader/EpubReader';
+// import EpubReader from './components/epubReader/EpubReader';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(ReduxThunk, sagaMiddleware));
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(reducers, applyMiddleware(ReduxThunk, sagaMiddleware));
 
-sagaMiddleware.run(sagas);
+// sagaMiddleware.run(sagas);
 
 // Use Apollo client
 // const client = new ApolloClient({
@@ -61,82 +64,42 @@ sagaMiddleware.run(sagas);
 //   cache: new InMemoryCache(),
 // });
 
-// Use Apollo boost
-const client = new ApolloClient({
-  uri: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/graphql' : '',
-});
+// // Use Apollo boost
+// const client = new ApolloClient({
+//   uri: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/graphql' : '',
+// });
 
-client
-  .query({
-    query: gql`
-      query {
-        testMovies(limit: 3, rating: 9) {
-          id
-          title
-          rating
-        }
-        testMovie(id: 15553) {
-          title
-          summary
-        }
-      }
-    `,
-  })
-  .then(result => console.log('TEST', result));
+// client
+//   .query({
+//     query: gql`
+//       query {
+//         testMovies(limit: 3, rating: 9) {
+//           id
+//           title
+//           rating
+//         }
+//         testMovie(id: 15553) {
+//           title
+//           summary
+//         }
+//       }
+//     `,
+//   })
+//   .then(result => console.log('TEST', result));
 
 function App() {
-  const [selectedItemsParent, setSelectedItemsParent] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
+  // const [selectedItemsParent, setSelectedItemsParent] = useState([]);
+  // const [selectedItems, setSelectedItems] = useState([]);
 
-  const handleSelectedCategories = (selectedIdListOnlyParent, selectedIdList) => {
-    setSelectedItemsParent(selectedIdListOnlyParent);
-    setSelectedItems(selectedIdList);
-  };
+  // const handleSelectedCategories = (selectedIdListOnlyParent, selectedIdList) => {
+  //   setSelectedItemsParent(selectedIdListOnlyParent);
+  //   setSelectedItems(selectedIdList);
+  // };
 
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <div className="App">
-          <PropsComp />
-          <ParentComp>
-            <ChildComp />
-          </ParentComp>
-          <MultiSelector
-            categories={TestMultiCategories}
-            initialSelectedIds={initialSelectedIds}
-            exceptionIds={exceptionIds}
-            notSelectionIds={notSelectionIds}
-            handleSelectedCategories={handleSelectedCategories}
-          >
-            <TestMultiCheckItem/>
-          </MultiSelector>
-          <div>
-            {'Only parent: '}{
-              selectedItemsParent.map((id) => {
-                return `${id}, `;
-              })
-            }
-          </div>
-          <div>
-            {'Whole selected: '}{
-              selectedItems.map((id) => {
-                return `${id}, `;
-              })
-            }
-          </div>
-          <CountComponent />
-          <CountFCComponent />
-          {/* <ApolloTestQuery /> */}
-          {/* <ApolloTest /> */}
-          {/* <ApolloTestDelay /> */}
-          {/* <ApolloTestMutation /> */}
-          {/* <ApolloTestMutationUpdate /> */}
-          <ApolloTestQueryAndMutation />
-          <ApolloTestQueryAndMutationUpdate />
-          <EpubReader />
-        </div>
-      </Provider>
-    </ApolloProvider>
+    <>
+      <ReactTreeMultiSelector />
+    </>
   );
 }
 
